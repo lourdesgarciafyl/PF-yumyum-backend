d3ddb0fa62dd58416a30ab9c9bef5977806f3225
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-21 13:47:13 -0300

    se iporta express y cambian los comandos de package json, ahora funciona npm run dev

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+
+dotenv.config();
+const app = express();
+app.set("PORT", process.env.PORT || 4000)
+app.listen(app.get("PORT"), () =>{
+    console.log("Estoy en el puerto "+ app.get("PORT"))
+})
