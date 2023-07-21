6165060cdf85d6c12cb52b19cbb092ada809b482
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-21 13:54:27 -0300

    se agregan los middlewares

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ app.set("PORT", process.env.PORT || 4000)
 app.listen(app.get("PORT"), () =>{
     console.log("Estoy en el puerto "+ app.get("PORT"))
 })
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+app.use(cors()); 
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, `/public`)))
