2c83b92cc87a46a8acbddbbec44f279b6ec4f560
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-23 20:59:36 -0300

    se agrega el controller para obtener una lista de productos, se agrega esta peticion de tipo get a las routes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
-import {crearProducto } from "../controllers/productos.controllers";
+import {crearProducto, obtenerListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
-router.route("/productos").post(validarProducto, crearProducto)
+router.route("/productos").post(validarProducto, crearProducto).get(obtenerListaProductos)
 
 // otra ruta para las peticiones faltantes
 
