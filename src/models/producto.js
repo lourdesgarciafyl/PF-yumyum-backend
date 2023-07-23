28a3493238ff71e7c3c0558f3301ac4d015e9a97
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-23 18:44:56 -0300

    se agrega el modelo producto considerando las propiedades que usamos en db json

---
+++ src/models/producto.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto:{
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 50
+    },
+    precio:{
+        type: Number,
+        required: true,
+        min: 100,
+        max: 10000
+    },
+    imagen:{
+        type: String,
+        required: true
+    },
+    detalle:{
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 500        
+    },
+    estado:{
+        type: String,
+        required: true
+    },
+    categoria:{
+        type: String,
+        required: true
+    }
+})
+
+const Producto = mongoose.model(`producto`, productoSchema) 
