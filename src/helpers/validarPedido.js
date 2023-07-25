64c55b6c7fec3f10e0ea7e6dda02d80cea425408
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-25 15:45:57 -0300

    se agregan las validaciones del pedido para cada campo; además se modifica el minimo y maximo del precioTotal y del subtotal en modelo Pedido

---
+++ src/helpers/validarPedido.js
@@ -0,0 +1,36 @@
+import resultadoValidacion from "./resultadoValidacion";
+import { check } from "express-validator";
+
+const validarPedido = [
+    check("usuario")
+    .notEmpty()
+    .withMessage("El usuario es un dato obligatorio")
+    .isString(),
+    check("fechaPedido")
+    .notEmpty()
+    .withMessage("La fecha es un dato obligatorio.")
+    .isDate(),
+    check("productos")
+    .notEmpty()
+    .withMessage("Debe ingresar productos al pedido.")
+    .isArray(),
+    check("estado")
+    .notEmpty()
+    .withMessage("El estado es un dato obligatorio")
+    .isIn(["En proceso","Entregado"])
+    .withMessage("Debe elegir una opción válida"),
+    check("precioTotal")
+    .notEmpty()
+    .withMessage("El precio total es un dato obligatorio")
+    .custom((value) => {
+        if(value >= 100 && value <= 500000){
+            return true;
+        } else {
+            throw new Error("El precio debe entre $100 y $10000")
+        }
+    }),
+   
+    (req, res, next) => {resultadoValidacion(req, res, next)}
+]
+
+export default validarPedido;
