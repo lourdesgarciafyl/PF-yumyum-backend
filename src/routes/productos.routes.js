17350f468435f807b30db7a8821e1deaf8b84bde
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-25 10:28:24 -0300

    se crea una funcion para validar el estado del producto, y se la agrega a las rutas para activar y desactivar un producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
+import validarEstadoProducto from "../helpers/validarEstadoProducto";
 import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 router.route("/").post(validarProducto, crearProducto).get(obtenerListaProductos)
 router.route("/:id").put(validarProducto, editarProducto)
 router.route("/categoria/:categoria").get(consultaProductosPorCategoria)
-router.route("/activar/:id").put(activarProducto)
-router.route("/desactivar/:id").put(desactivarProducto)
+router.route("/activar/:id").put(validarEstadoProducto, activarProducto)
+router.route("/desactivar/:id").put(validarEstadoProducto, desactivarProducto)
 
 export default router;
