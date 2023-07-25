782d22ebc4014bac88099805d9996dbb33971985
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-25 10:02:58 -0300

    se agregan los controllers para activar y desactivar producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
-import {consultaProductosPorCategoria, crearProducto, editarProducto, obtenerListaProductos } from "../controllers/productos.controllers";
+import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 router.route("/").post(validarProducto, crearProducto).get(obtenerListaProductos)
-router.route("/:id").put(validarProducto, editarProducto)
+router.route("/:id").put(validarProducto, editarProducto).put(validarProducto, activarProducto).put(validarProducto, desactivarProducto)
 router.route("/categoria/:categoria").get(consultaProductosPorCategoria)
 
 export default router;
