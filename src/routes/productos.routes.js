318826630227000d159b34953e6da12fdc10ff91
Agustin Baza <agustinbaza10@gmail.com>
2023-07-25 20:40:06 -0300

    Se agrego tambien el borrarProducto, funciona correctamente, probado en postman

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
 import validarEstadoProducto from "../helpers/validarEstadoProducto";
-import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos, obtenerProducto } from "../controllers/productos.controllers";
+import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos, obtenerProducto, borrarProducto } from "../controllers/productos.controllers";
 
 const router = Router();
 router.route("/").post(validarProducto, crearProducto).get(obtenerListaProductos)
-router.route("/:id").get(obtenerProducto).put(validarProducto, editarProducto)
+router.route("/:id").get(obtenerProducto).delete(borrarProducto).put(validarProducto, editarProducto)
 router.route("/categoria/:categoria").get(consultaProductosPorCategoria)
 router.route("/activar/:id").put(validarEstadoProducto, activarProducto)
 router.route("/desactivar/:id").put(validarEstadoProducto, desactivarProducto)
