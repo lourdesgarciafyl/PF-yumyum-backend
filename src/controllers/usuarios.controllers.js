49001d86d1e253f282644a51e77023146ce7a21a
Agustin Baza <agustinbaza10@gmail.com>
2023-07-26 15:42:46 -0300

    Agregue logica sobre borrarUsuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -33,3 +33,25 @@ export const crearUsuario = async (req, res) => {
     });
   }
 };
+
+export const borrarUsuario = async (req, res) => {
+  try { 
+
+    // Aqui verificamos si el usuario existe en la BD
+     const usuario = await Usuario.findById(req.params.id);
+      if (!usuario) {
+          return res.status(404).json({
+              mensaje: "El usuario no fue encontrado.",
+          });
+      } // Borramos el usuario de la BD
+      await Usuario.findByIdAndDelete(req.params.id);
+      res.status(200).json({
+          mensaje: "Usuario eliminado exitosamente."
+      });
+  } catch (error) {
+      console.log(error);
+      res.status(400).json({
+          mensaje: "No se pudo eliminar el usuario.",
+      });
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import validarUsuario from "../helpers/validarUsuario";
-import {crearUsuario} from "../controllers/usuarios.controllers";
+import {borrarUsuario, crearUsuario} from "../controllers/usuarios.controllers";
 
 const router = Router();
 router.route("/nuevo").post(validarUsuario, crearUsuario)
+router.route("/:id").delete(borrarUsuario)
 export default router;
