e332554b939f78fd2951c8168af089e5ac80a218
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-26 18:53:15 -0300

    se prueban opciones para intentar que funcione el crear pedido, sigue sin funcionar

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,16 +1,17 @@
 import Pedido from "../models/pedido";
 
 export const crearPedido = async (req, res) => {
-    try{
-        const pedidoNuevo = new Pedido(req.body);
-        await pedidoNuevo.save();
-        res.status(201).json({
-            mensaje: "El pedido fue creado correctamente."
-        });
+    try {
+      const pedidoNuevo = new Pedido(req.body);
+      await pedidoNuevo.save();
+      res.status(201).json({
+        mensaje: 'El pedido fue creado correctamente.',
+      });
     } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            mensaje: "Error. No se pudo crear el nuevo pedido."
-        });
+      console.log(error);
+      res.status(400).json({
+        mensaje: 'Error no se pudo crear un pedido',
+      });
     }
-};
+  };
+  
