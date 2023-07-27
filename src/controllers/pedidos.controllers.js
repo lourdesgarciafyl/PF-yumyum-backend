a5cdb4353783bc1b76385f0ed1c04e102d7f0569
Agustin Baza <agustinbaza10@gmail.com>
2023-07-27 15:05:28 -0300

    Se probo el editar estado del pedido en postman, funciona correctamente

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,115 +1,112 @@
 import Pedido from "../models/pedido";
 
 export const crearPedido = async (req, res) => {
-    try {
-      const pedidoNuevo = new Pedido(req.body);
-      await pedidoNuevo.save();
-      res.status(201).json({
-        mensaje: 'El pedido fue creado correctamente.',
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({
-        mensaje: 'Error no se pudo crear un pedido',
-      });
-    }
-  };
-  
-  export const obtenerListaPedidos = async (req, res) => {
-    try {
-      //buscar en la BD la collection de pedidos
-      const pedidos = await Pedido.find()
-        .populate({
-          path: 'productos.producto',
-          select: '-_id -__v', // Opcional: selecciona los campos que deseas excluir, como _id y __v
-        })
-        .populate({
-          path: 'usuario',
-          select: '-_id -password -estado -perfil -__v', //  Opcional: selecciona los campos que deseas excluir, como _id y __v -password -estado -perfil
-        });
-      res.status(200).json(pedidos);
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: 'Error al intentar listar los pedidos',
-      });
-    }
-  };
-  
-  export const obtenerPedido = async (req, res) =>{
-    try{
-       const pedido = await Pedido.findById(req.params.id).populate({
-        path: 'productos.producto',
-        select: '-_id -__v', 
+  try {
+    const pedidoNuevo = new Pedido(req.body);
+    await pedidoNuevo.save();
+    res.status(201).json({
+      mensaje: "El pedido fue creado correctamente.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "Error no se pudo crear un pedido",
+    });
+  }
+};
+
+export const obtenerListaPedidos = async (req, res) => {
+  try {
+    //buscar en la BD la collection de pedidos
+    const pedidos = await Pedido.find()
+      .populate({
+        path: "productos.producto",
+        select: "-_id -__v", // Opcional: selecciona los campos que deseas excluir, como _id y __v
       })
       .populate({
-        path: 'usuario',
-        select: '-_id -password -estado -perfil -__v', 
+        path: "usuario",
+        select: "-_id -password -estado -perfil -__v", //  Opcional: selecciona los campos que deseas excluir, como _id y __v -password -estado -perfil
       });
-       res.status(200).json(pedido);
-    }catch(error){
-        console.log(error)
-        res.status(404).json({
-            mensaje: "Error, no se pudo obtener el pedido."
-        })
-    }
+    res.status(200).json(pedidos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar listar los pedidos",
+    });
   }
+};
 
-  export const entregarPedido = async (req, res) => {
-    const idPedido = req.params.id;
-    try {
-      const pedido = await Pedido.findById(idPedido);
-      if (!pedido) {
-        return res.status(404).json({ error: 'Pedido no encontrado' });
-      }
-  
-  
-      if (pedido.estado === 'Entregado') {
-        return res
-          .status(404)
-          .json({ error: 'El pedido ya se encuentra en Entregado' });
-      }
-  
-  
-      pedido.estado = 'Entregado';
-      await pedido.save();
-      res.status(200).json({
-        mensaje: 'Se entregó el pedido correctamente.',
+export const obtenerPedido = async (req, res) => {
+  try {
+    const pedido = await Pedido.findById(req.params.id)
+      .populate({
+        path: "productos.producto",
+        select: "-_id -__v",
+      })
+      .populate({
+        path: "usuario",
+        select: "-_id -password -estado -perfil -__v",
       });
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: 'Error, no se pudo pasar a entregado el pedido.',
-      });
+    res.status(200).json(pedido);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error, no se pudo obtener el pedido.",
+    });
+  }
+};
+
+export const entregarPedido = async (req, res) => {
+  const idPedido = req.params.id;
+  try {
+    const pedido = await Pedido.findById(idPedido);
+    if (!pedido) {
+      return res.status(404).json({ error: "Pedido no encontrado" });
     }
-  };
-  
-  
-  export const pedidoEnProceso = async (req, res) => { // En proceso
-    const idPedido = req.params.id;
-    try {
-      const pedido = await Pedido.findById(idPedido);
-      if (!pedido) {
-        return res.status(404).json({ error: 'Pedido no encontrado' });
-      }
-  
-  
-      if (pedido.estado === 'En proceso') {
-        return res
-          .status(404)
-          .json({ error: 'El pedido ya se encuentra en proceso' });
-      }
-  
-  
-      pedido.estado = 'En proceso';
-      await pedido.save();
-      res.status(200).json({
-        mensaje: 'El pedido esta en proceso.',
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: 'Error, no se pudo pasar a "en proceso" el pedido.',
-      });
+
+    if (pedido.estado === "Entregado") {
+      return res
+        .status(404)
+        .json({ error: "El pedido ya se encuentra en Entregado" });
     }
-  };
+
+    pedido.estado = "Entregado";
+    await pedido.save();
+    res.status(200).json({
+      mensaje: "Se entregó el pedido correctamente.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error, no se pudo pasar a entregado el pedido.",
+    });
+  }
+};
+
+export const pedidoEnProceso = async (req, res) => {
+  // En proceso
+  const idPedido = req.params.id;
+  try {
+    const pedido = await Pedido.findById(idPedido);
+    if (!pedido) {
+      return res.status(404).json({ error: "Pedido no encontrado" });
+    }
+
+    if (pedido.estado === "En proceso") {
+      return res
+        .status(404)
+        .json({ error: "El pedido ya se encuentra en proceso" });
+    }
+
+    pedido.estado = "En proceso";
+    await pedido.save();
+    res.status(200).json({
+      mensaje: "El pedido esta en proceso.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: 'Error, no se pudo pasar a "en proceso" el pedido.',
+    });
+  }
+};
