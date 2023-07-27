50b14bada2ddbe1854b825eadb1354624003fec2
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-26 22:13:34 -0300

    se hizo la función de validarToken y se probó con el crearProducto que valide el token que viene por el req.header

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
 import validarEstadoProducto from "../helpers/validarEstadoProducto";
 import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos, obtenerProducto, borrarProducto } from "../controllers/productos.controllers";
+import validarJWT from "../helpers/tokenVerificacion";
 
 const router = Router();
-router.route("/").post(validarProducto, crearProducto).get(obtenerListaProductos)
+router.route("/").post([validarJWT, validarProducto], crearProducto).get(obtenerListaProductos)
 router.route("/:id").get(obtenerProducto).delete(borrarProducto).put(validarProducto, editarProducto)
 router.route("/categoria/:categoria").get(consultaProductosPorCategoria)
 router.route("/activar/:id").put(validarEstadoProducto, activarProducto)
