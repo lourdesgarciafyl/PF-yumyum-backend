b59b78292fdb5919d4d35f6eb5267ad5885dc219
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-27 08:30:55 -0300

    se agrega el controller registro

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {validarLogin, validarUsuario} from "../helpers/validarUsuario";
-import {borrarUsuario, crearUsuario, editarUsuario, loginUsuario, obtenerListaUsuarios, obtenerUsuario} from "../controllers/usuarios.controllers";
+import {borrarUsuario, crearUsuario, editarUsuario, loginUsuario, obtenerListaUsuarios, obtenerUsuario, registro} from "../controllers/usuarios.controllers";
 
 const router = Router();
 router.route("/").get(obtenerListaUsuarios)
 router.route("/login").post(validarLogin,loginUsuario)
 router.route("/nuevo").post(validarUsuario, crearUsuario)
+router.route("/registro").post(registro)
 router.route("/:id").delete(borrarUsuario).put(validarUsuario, editarUsuario).get(obtenerUsuario)
 export default router;
