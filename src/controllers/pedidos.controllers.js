86909eb8b2232312ae680a4ac95652f415683af1
Cristian Quiroga <cristianq3@gmail.com>
2023-07-27 12:18:04 -0300

    Se agregan rutas y borrarPedido

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -1,56 +1,78 @@
 import Pedido from "../models/pedido";
 
 export const crearPedido = async (req, res) => {
-    try {
-      const pedidoNuevo = new Pedido(req.body);
-      await pedidoNuevo.save();
-      res.status(201).json({
-        mensaje: 'El pedido fue creado correctamente.',
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({
-        mensaje: 'Error no se pudo crear un pedido',
-      });
-    }
-  };
-  
-  export const obtenerListaPedidos = async (req, res) => {
-    try {
-      //buscar en la BD la collection de pedidos
-      const pedidos = await Pedido.find()
-        .populate({
-          path: 'productos.producto',
-          select: '-_id -__v', // Opcional: selecciona los campos que deseas excluir, como _id y __v
-        })
-        .populate({
-          path: 'usuario',
-          select: '-_id -password -estado -perfil -__v', //  Opcional: selecciona los campos que deseas excluir, como _id y __v -password -estado -perfil
-        });
-      res.status(200).json(pedidos);
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: 'Error al intentar listar los pedidos',
-      });
-    }
-  };
-  
-  export const obtenerPedido = async (req, res) =>{
-    try{
-       const pedido = await Pedido.findById(req.params.id).populate({
-        path: 'productos.producto',
-        select: '-_id -__v', 
+  try {
+    const pedidoNuevo = new Pedido(req.body);
+    await pedidoNuevo.save();
+    res.status(201).json({
+      mensaje: "El pedido fue creado correctamente.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "Error no se pudo crear un pedido",
+    });
+  }
+};
+
+export const obtenerListaPedidos = async (req, res) => {
+  try {
+    //buscar en la BD la collection de pedidos
+    const pedidos = await Pedido.find()
+      .populate({
+        path: "productos.producto",
+        select: "-_id -__v", // Opcional: selecciona los campos que deseas excluir, como _id y __v
       })
       .populate({
-        path: 'usuario',
-        select: '-_id -password -estado -perfil -__v', 
+        path: "usuario",
+        select: "-_id -password -estado -perfil -__v", //  Opcional: selecciona los campos que deseas excluir, como _id y __v -password -estado -perfil
       });
-       res.status(200).json(pedido);
-    }catch(error){
-        console.log(error)
-        res.status(404).json({
-            mensaje: "Error, no se pudo obtener el pedido."
-        })
-    }
+    res.status(200).json(pedidos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar listar los pedidos",
+    });
   }
+};
+
+export const obtenerPedido = async (req, res) => {
+  try {
+    const pedido = await Pedido.findById(req.params.id)
+      .populate({
+        path: "productos.producto",
+        select: "-_id -__v",
+      })
+      .populate({
+        path: "usuario",
+        select: "-_id -password -estado -perfil -__v",
+      });
+    res.status(200).json(pedido);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error, no se pudo obtener el pedido.",
+    });
+  }
+};
+
+export const borrarPedido = async (req, res) => {
+  try {
+    // Aqui verificamos si el pedido existe en la BD
+    const pedido = await Pedido.findById(req.params.id);
+    if (!pedido) {
+      return res.status(404).json({
+        mensaje: "El pedido no fue encontrado.",
+      });
+    } // Borramos el usuario de la BD
+    await pedido.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      mensaje: "Pedido eliminado exitosamente.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo eliminar el pedido.",
+    });
+  }
+};
--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import validarPedido from "../helpers/validarPedido";
-import { crearPedido, obtenerListaPedidos, obtenerPedido } from "../controllers/pedidos.controllers";
+import { crearPedido, obtenerListaPedidos, obtenerPedido, borrarPedido } from "../controllers/pedidos.controllers";
 
 const router = Router();
 router.route("/").post(validarPedido, crearPedido).get(obtenerListaPedidos)
-router.route("/:id").get(obtenerPedido)
+router.route("/:id").get(obtenerPedido).delete(borrarPedido)
 
 export default router
