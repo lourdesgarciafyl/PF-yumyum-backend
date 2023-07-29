519ff85218f211c35b0f769d2f2ae52bfcac7c7f
Agustin Baza <agustinbaza10@gmail.com>
2023-07-29 13:51:07 -0300

    Se probo la correccion en postman correctamente

--- src/helpers/validarEstadoPedido.js
+++ src/helpers/validarEstadoPedido.js
@@ -2,15 +2,15 @@ import { check } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion";
 
 const validarEstadoPedido = [
-    check("estado")
-        .notEmpty()
-        .withMessage("El estado es un dato obligatorio")
-        .isIn(["En proceso", "Entregado"])
-        .withMessage("Debe elegir una opción válida"),
+  check("estado")
+    .notEmpty()
+    .withMessage("El estado es un dato obligatorio")
+    .isIn(["En proceso", "Entregado"])
+    .withMessage("Debe elegir una opción válida"),
 
-    (req, res, next) => {
-        resultadoValidacion(req, res, next);
-    }
+  (req, res, next) => {
+    resultadoValidacion(req, res, next);
+  },
 ];
 
 export default validarEstadoPedido;
