19f1b98904cb223a5a611139ca952255631f3ea0
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-30 10:35:13 -0300

    se agrega en routes la peticion para poder eliminar un pedido

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import validarPedido from "../helpers/validarPedido";
-import { crearPedido, entregarPedido, obtenerListaPedidos, obtenerPedido, pedidoEnProceso } from "../controllers/pedidos.controllers";
+import { borrarPedido, crearPedido, entregarPedido, obtenerListaPedidos, obtenerPedido, pedidoEnProceso } from "../controllers/pedidos.controllers";
 
 const router = Router();
 router.route("/").post(validarPedido, crearPedido).get(obtenerListaPedidos)
-router.route("/:id").get(obtenerPedido)
+router.route("/:id").get(obtenerPedido).delete(borrarPedido)
 router.route("/enproceso/:id").put(pedidoEnProceso)
 router.route("/entregado/:id").put(entregarPedido)
 
