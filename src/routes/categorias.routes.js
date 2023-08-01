b02411755283dcd98352a478c9b8b5979a1c0259
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-31 22:19:05 -0300

    se agregaron las validaciones de JWT a crearCategoria

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { crearCategoria, obtenerListaCategorias, obtenerListaCategoriasActivas } from '../controllers/categorias.controllers';
 import validarCategoria from '../helpers/validarCategoria';
+import validarJWT from '../helpers/tokenVerificacion';
 const router = Router();
-router.route('/').post(validarCategoria,crearCategoria).get(obtenerListaCategorias);
+router.route('/').post([validarJWT,validarCategoria],crearCategoria).get(obtenerListaCategorias);
 router.route('/activas').get(obtenerListaCategoriasActivas);
 
 export default router;
