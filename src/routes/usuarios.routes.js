bf8bab231a3c046662a12d20be580eeda9545228
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-01 15:19:31 -0300

    se agrega la validacion con token, se prueba en postman y funciona correctamente

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {validarLogin, validarRegistro, validarUsuario} from "../helpers/validarUsuario";
-import {borrarUsuario, crearUsuario, editarUsuario, loginUsuario, obtenerListaUsuarios, obtenerUsuario, registro} from "../controllers/usuarios.controllers";
+import {borrarUsuario, cambiarPassword, crearUsuario, editarUsuario, loginUsuario, obtenerListaUsuarios, obtenerUsuario, registro} from "../controllers/usuarios.controllers";
 import validarJWT from "../helpers/tokenVerificacion";
 
 const router = Router();
@@ -9,4 +9,6 @@ router.route("/registro").post(validarRegistro, registro)
 router.route("/login").post(validarLogin,loginUsuario)
 router.route("/nuevo").post([validarJWT, validarUsuario], crearUsuario)
 router.route("/:id").delete(validarJWT, borrarUsuario).put(validarJWT, editarUsuario).get(validarJWT, obtenerUsuario)
+router.route("/nuevopassword/:id").put(validarJWT, cambiarPassword)
+
 export default router;
