f4c514d04796f5fb0ab213e7e2c407fbc6047386
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-31 21:31:46 -0300

    se agregaron las validaciones de JWT a listadoPedidos admin, consulta de un pedido, cambiar estado de enproceso a entregado y viceversa

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import validarPedido from "../helpers/validarPedido";
 import { borrarPedido, crearPedido, entregarPedido, obtenerListaPedidos, obtenerPedido, pedidoEnProceso } from "../controllers/pedidos.controllers";
+import validarJWT from "../helpers/tokenVerificacion";
 
 const router = Router();
-router.route("/").post(validarPedido, crearPedido).get(obtenerListaPedidos)
-router.route("/:id").get(obtenerPedido).delete(borrarPedido)
-router.route("/enproceso/:id").put(pedidoEnProceso)
-router.route("/entregado/:id").put(entregarPedido)
+router.route("/").post(validarPedido, crearPedido).get(validarJWT,obtenerListaPedidos)
+router.route("/:id").get(validarJWT,obtenerPedido).delete(validarJWT,borrarPedido)
+router.route("/enproceso/:id").put(validarJWT,pedidoEnProceso)
+router.route("/entregado/:id").put(validarJWT,entregarPedido)
 
 export default router
