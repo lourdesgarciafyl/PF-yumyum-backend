c8c3cf0040ec1373e0576f91dc080d4b40522c50
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-04 17:06:56 -0300

    se agrega el controlador para listar los productos activos, igual al listar categorias activas

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import validarProducto from "../helpers/validarProducto";
-import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos, obtenerProducto, borrarProducto } from "../controllers/productos.controllers";
+import {activarProducto, consultaProductosPorCategoria, crearProducto, desactivarProducto, editarProducto, obtenerListaProductos, obtenerProducto, borrarProducto, obtenerProductosActivos } from "../controllers/productos.controllers";
 import validarJWT from "../helpers/tokenVerificacion";
 
 
 const router = Router();
 router.route("/").post([validarJWT, validarProducto], crearProducto).get(obtenerListaProductos)
+router.route("/activos").get(obtenerProductosActivos)
 router.route("/:id").get(obtenerProducto).delete(validarJWT, borrarProducto).put([validarJWT, validarProducto], editarProducto)
 router.route("/categoria/:categoria").get(consultaProductosPorCategoria)
 router.route("/activar/:id").put(validarJWT, activarProducto)
